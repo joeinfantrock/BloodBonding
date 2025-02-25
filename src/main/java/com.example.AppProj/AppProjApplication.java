00ccb43b55698b2294be765f4cbd28d334c42423
Joe Infant <a.joeinfant@gmail.com>
2025-02-25 17:54:29 +0530

    Update AppProjApplication.java

--- src/main/java/com.example.AppProj/AppProjApplication.java
+++ src/main/java/com.example.AppProj/AppProjApplication.java
@@ -1,1 +1,13 @@
+package com.example.AppProj;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppProjApplication.class, args);
+	}
+
+}
